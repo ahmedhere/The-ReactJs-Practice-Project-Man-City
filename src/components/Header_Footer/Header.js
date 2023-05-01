640afb0f2e9dd063cf8999f4e7a4d8a2b39adeaa
ahmedhere <104449961+ahmedhere@users.noreply.github.com>
2023-05-01 21:07:24 +0500

    Header created

--- src/components/Header_Footer/Header.js
+++ src/components/Header_Footer/Header.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
+import { AppBar, Toolbar, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: "#98c5e9",
+          boxShadow: "none",
+          padding: "10px 0px",
+          borderBottom: "2px solid #00285e",
+        }}
+      >
+        <Toolbar style={{ display: "flex" }}>
+          <div style={{ flexGrow: "1" }}>
+            <div className="header_logo">Logo</div>
+            <Link to="/the_team">
+              <Button color="inherit">The team</Button>
+            </Link>
+            <Link to="/the_matches">
+              <Button color="inherit">Matches</Button>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+    );
   }
 }
