e8c502b0e8a0bd4448d540d888c1fd0e7f2e9974
ahmedhere <104449961+ahmedhere@users.noreply.github.com>
2023-05-01 11:37:29 +0500

    Router implemented in the app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Routes from "./routes";
+import Routes from "./route";
 import { BrowserRouter } from "react-router-dom";
 import "./Resources/css/app.css";
 
